c3f779e21846bfaaf280262f3ad1277dd71b138d
Daniel Andersen <dan.musik@gmail.com>
2021-08-29 23:55:55 -0300

    fix cors

--- src/main/java/com/vnoxiaene/financialreconciliation/controller/FileUploadController.java
+++ src/main/java/com/vnoxiaene/financialreconciliation/controller/FileUploadController.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.Arrays;
 
 @Log4j2
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/files")
 public class FileUploadController {
